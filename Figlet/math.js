b59b96cec8c29774ddf2e78573eff0a70d38bf2d
varun-2024 <v.talwar2024@gmail.com>
2025-03-31 16:30:12 +0100

    Revesion

---
+++ Figlet/math.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+const calculator = {
+  sum,
+  sub,
+  mul,
+  div,
+};
+
+module.exports = calculator;
---
+++ Figlet/script.js
@@ -0,0 +1,4 @@
+const cal = require("./math");
+const fruits = require("../sidefolder");
+//console.log(cal);
+console.log(fruits[0].name);
