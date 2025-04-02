522aa0e96bb7807edd3d09ea6e5030310d29e66c
varun-2024 <v.talwar2024@gmail.com>
2025-04-02 13:29:34 +0100

    Add footer and head includes to EJS views; implement dice roll functionality in app.js

--- Backend/Express/EJS/public/js/app.js
+++ Backend/Express/EJS/public/js/app.js
@@ -4,3 +4,16 @@ for (btn of btns) {
     console.log("Button was clicked");
   });
 }
+
+let rollbtn = document.querySelector(".rollbtn");
+let newRoll = document.querySelector(".newRoll");
+if (rollbtn) {
+  rollbtn.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    console.log("Random number generated:", randomNumber);
+    newRoll.textContent = "New Roll: " + randomNumber;
+    if (randomNumber === 6) {
+      window.location.reload();
+    }
+  });
+}
